d6669c59d166fcc4ae673cc33725d2600dc43e84
Pawel Gaca <pawecio33359@gmail.com>
2026-01-06 13:43:34 +0100

    supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,11 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://jxanprnhzlrromuxiuwr.supabase.co";
-let supabaseKey;
-if (import.meta.env.NETLIFY === "true") {
-  supabaseKey = process.env.VITE_SUPABASE_KEY;
-} else {
-  supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-}
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4YW5wcm5oemxycm9tdXhpdXdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMTg0NDMsImV4cCI6MjA1NDY5NDQ0M30._B14NixVX0cnkeccKy7f3NOWtoowrcF7drHQlvCXCfE";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
